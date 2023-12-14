9519486a0206d3fa9fce2d99dfca1a81c9c5827e
Darius <dariuszdroba@gmail.com>
2023-12-14 15:32:05 +0200

    Anagram,  .Values.All determines wether all elements respect a certain condition

---
+++ Anagram/Program.cs
@@ -0,0 +1,45 @@
+﻿//Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+//Example 1:
+
+//Input: s = "anagram", t = "nagaram"
+//Output: true
+
+static bool IsAnagram(string s, string t)
+{
+    if (s.Length != t.Length) return false;
+    Dictionary<char,int> sHashSet = new Dictionary<char, int>();
+    for(int i=0; i<s.Length; i++)
+    {
+        if (sHashSet.ContainsKey(s[i]))
+        {
+            sHashSet[s[i]]++;
+        }
+        else
+        {
+            sHashSet.Add(s[i], 1);
+        }
+    }
+    for(int i=0;i<s.Length;i++)
+    {
+        if (sHashSet.ContainsKey(t[i]))
+        {
+            sHashSet[t[i]]--;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    bool contains = sHashSet.Values.All(v => v==0);
+
+    return contains;
+  
+}
+string s = "aa";
+string t = "bb";
+IsAnagram(s, t);
---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,26 @@
+﻿//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+//Example 1:
+
+//Input: nums = [1, 2, 3, 1]
+//Output: true
+
+//Example 2:
+
+//Input: nums = [1, 2, 3, 4]
+//Output: false
+
+static bool ContainsDuplicate(int[] nums)
+{
+    HashSet<int> result = new HashSet<int>();
+    for(int i = 0; i < nums.Length; i++)
+    {
+        if (result.Contains(nums[i])) return true;
+        result.Add(nums[i]);
+    }
+    return false;
+}
+int[] nums = { 1, 2, 3, 4, 1 };
+ContainsDuplicate(nums);
