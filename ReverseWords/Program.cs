07bc749dd343be8991273e58923ee45bf82fe90e
Darius <dariuszdroba@gmail.com>
2023-12-14 14:56:07 +0200

    ProductExceptSelf O(N) Time, O(1) Space, not considering output array.

---
+++ ReverseWords/Program.cs
@@ -0,0 +1,10 @@
+﻿static string ReverseWords(string s)
+{
+    var outp = s.Trim().Split(" ");
+    string output = "";
+    for (int i =outp.Length - 1; i > 0; i--)
+        output +=outp[i] ;
+
+return output;
+    
+}
--- TwoSumDictionary/Program.cs
+++ TwoSumDictionary/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int[] TwoSum(int[] nums, int target)
+{
+    Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+    for(int i=0; i < nums.Length;i++)
+    {
+        if (keyValuePairs.ContainsKey(target - nums[i]))
+            return new int[] { i, keyValuePairs[target - nums[i]] };
+        if (!keyValuePairs.ContainsKey(nums[i]))
+            keyValuePairs.Add(nums[i], i);
+    }
+    return new int[1];
+}
+int[] nums = { 3, 2, 4 };
+var a = TwoSum(nums, 6);
