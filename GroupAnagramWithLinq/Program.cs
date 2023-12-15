cfe7f14cb97bc718cb4113828ecf83aa4aebc81e
Darius <dariuszdroba@gmail.com>
2023-12-15 21:15:15 +0200

    sudokuToDO

---
+++ GroupAnagramWithLinq/Program.cs
@@ -0,0 +1,28 @@
+﻿//given an array of strings strs, group the anagrams together. you can return the answer in any order.
+
+//an anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+//example 1:
+
+//input: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+//output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+//example 2:
+
+//input: strs = [""]
+//output: [[""]]
+
+
+static IList<IList<string>> GroupAnagrams(string[] strs)
+{
+    return strs
+        .GroupBy(s => new string(s.OrderBy(c => c).ToArray()))
+        .Select(g => g.ToList() as IList<string>)
+        .ToList();
+}
+string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+GroupAnagrams(strs);
+Console.WriteLine("");
+
---
+++ TopKFrequentInt/Program.cs
@@ -0,0 +1,36 @@
+﻿//given an integer array nums and an integer k, return the k most frequent elements. you may return the answer in any order.
+
+
+
+//example 1:
+
+//input: nums = [1, 1, 1, 2, 2, 3], k = 2
+//output: [1,2]
+//example 2:
+
+//input: nums = [1], k = 1
+//output: [1]
+
+static int[] TopKFrequent(int[] nums, int k)
+{
+    int[] freqArr = new int[nums.Length*10];
+    int[] outputArr = new int[k];
+
+    for(int i=0; i<nums.Length; i++)
+    {
+         freqArr[nums[i]]++;
+        
+    }
+    Array.Sort(freqArr);
+    int cnt = 0;
+    for(int j = freqArr.Length-1; j >= freqArr.Length-k; j--)
+    {
+        outputArr[cnt] = freqArr[j];
+        cnt++;
+    }
+    return outputArr;
+}
+int[] nums = {1,1,1,2,2,3,4,5};
+// freq = {3,1,1,2,1}
+int k = 2;
+TopKFrequent(nums, k);
