fde0e99248a8e9f3066ec5fe4c6e7bd3a540cec1
Darius <dariuszdroba@gmail.com>
2023-12-19 21:13:17 +0200

    satckorsum

---
+++ MinStack/MinStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinStack
+{
+    public class MinStack
+    {
+        private Stack<int> _minStack;
+        private Stack<int> _minCounterStack;
+        private int min;
+        public MinStack()
+        {
+            _minStack = new Stack<int>();
+            _minCounterStack = new Stack<int>();
+        }
+
+        public void Push(int val)
+        {
+            if(_minStack.Count == 0) 
+            {
+                _minCounterStack.Push(val);
+                min = val;
+            }
+            else if(val < min)
+            {
+                _minCounterStack.Push(val);
+                min = val;
+            }
+            _minStack.Push(val);
+        }
+
+        public void Pop()
+        {
+            if(_minCounterStack.Count != 0)
+            if(_minStack.Pop().Equals(_minCounterStack.Peek()))
+            {
+                _minCounterStack.Pop();
+                min = _minCounterStack.Peek();
+            }
+        }
+
+        public int Top()
+        {
+            return _minStack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return min;
+        }
+    }
+}
---
+++ ValidParantheses/Program.cs
@@ -0,0 +1,51 @@
+﻿static bool IsValid(string s)
+{
+    Stack<char> round = new Stack<char>();
+    Stack<char> square = new Stack<char>();
+    Stack<char> brackets = new Stack<char>();
+    for(int i = 0; i < s.Length; i++)
+    {
+        if (s[i] == '(')
+        {
+            round.Push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            if(round.Count != 0)
+            round.Pop();
+            else
+            {
+                return false;
+            }
+        }
+       if(s[i] == '{')
+        {
+            brackets.Push(s[i]);
+        }
+       else if (s[i] =='}')
+        {
+            if(brackets.Count !=0)
+            brackets.Pop();
+            else
+            {
+                return false;
+            }
+        }
+       if(s[i] == '[')
+        {
+
+            square.Push(s[i]);
+        }
+       else if (s[i] == ']')
+        {
+            if (square.Count != 0)
+                square.Pop();
+            else
+                return false;
+        }
+    }
+    var sum = square.Count + brackets.Count + round.Count;
+        return sum == 0;    
+}
+string s = "(]";
+IsValid(s);
