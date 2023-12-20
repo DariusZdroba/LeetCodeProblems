0c279a6986646b322e97ba9a47572a984b4f1d42
Darius <dariuszdroba@gmail.com>
2023-12-20 15:03:44 +0200

    notYetFullSudoku

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -38,18 +38,43 @@
 //, [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 //Output: false
 //Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
-static bool IsValidSudoku(char[][] board)
+static bool IsValidSudoku(char[,] board)
 {
-    for (int i = 0; i<9; i++)
+    Dictionary<int, List<char>> rowDict = new Dictionary<int, List<char>>();
+    Dictionary<int, List<char>> colDict = new Dictionary<int, List<char>>();
+    Dictionary<List<int>, List<char>> squareDict = new Dictionary<List<int>, List<char>>();
+    for(int i=0;i<9;i++)
     {
-        Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
-        for (int j = 0; j < 9; j++)
+        for (int j = 0; j < 9;j++)
         {
-            if (keyValuePairs.ContainsKey(board[i][j]))
-                return false;
-            keyValuePairs[board[i][j]] = 1;
+            if (board[i, j] == '.') continue;
+            if (rowDict.ContainsKey(i))
+            {
+                if (rowDict[i].Contains(board[i, j]))
+                    return false;
+            }
+            if (colDict.ContainsKey(j))
+            {
+                if (colDict[j].Contains(board[i, j]))
+                    return false;
+            }
+            List<int> ints = new List<int> { i /3, j /3 };
+            if (squareDict.ContainsKey(ints))
+            {
+                if (squareDict[ints].Contains(board[i, j]))
+                    return false;
+            }
+                if(!rowDict.ContainsKey(i)) rowDict.Add(i, new List<char>());
+                if(!colDict.ContainsKey(j)) colDict.Add(j, new List<char>());
+                if(!squareDict.ContainsKey(ints)) squareDict.Add(ints, new List<char>());
+                rowDict[i].Add(board[i, j]);
+                colDict[j].Add(board[i, j]);
+                squareDict[ints].Add(board[i, j]);
         }
     }
-
+    return true;
 }
 
+char[,] board = { { '.', '.', '.', '.', '5', '.', '.', '1', '.' }, { '.', '4', '.', '3', '.', '.', '.', '.', '.' }, { '.', '.', '.', '.', '.', '3', '.', '.', '1' }, { '8', '.', '.', '.', '.', '.', '.', '2', '.' }, { '.', '.', '2', '.', '7', '.', '.', '.', '.' }, { '.', '1', '5', '.', '.', '.', '.', '.', '.' }, { '.', '.', '.', '.', '.', '2', '.', '.', '.' }, { '.', '2', '.', '9', '.', '.', '.', '.', '.' }, { '.', '.', '4', '.', '.', '.', '.', '.', '.' } };
+
+IsValidSudoku(board);
