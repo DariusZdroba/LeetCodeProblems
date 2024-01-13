d4d872989489579a96c9e9098cf1e22f8eba6e99
Darius <dariuszdroba@gmail.com>
2024-01-13 19:22:38 +0200

    TODO: KokoEatingBananas refactoring using BinarySearch

---
+++ Koko Eating Bananas/Program.cs
@@ -0,0 +1,59 @@
+﻿//Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas.The guards have gone and will come back in h hours.
+
+//Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+//Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+//Return the minimum integer k such that she can eat all the bananas within h hours.
+
+
+
+//Example 1:
+
+//Input: piles = [3, 6, 7, 11], h = 8
+//Output: 4
+//Example 2:
+
+//Input: piles = [30, 11, 23, 4, 20], h = 5
+//Output: 30
+//Example 3:
+
+//Input: piles = [30, 11, 23, 4, 20], h = 6
+//Output: 23
+
+int MinEatingSpeed(int[] piles, int h)
+{
+    int max = piles.Max();
+    int div = 1;
+    long sum = h+1;
+    //TODO 
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //Try binary search solution, instead of setting div from 1 trough max of piles, go for a 
+    //divide and conquer solution, going to max/2, if larger, difference of max and max/2  / 2  and so on.
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    while (div <= max && sum > h)
+    {
+        sum = 0;
+        for(int i=0; (i< piles.Length) && sum<=h ; i++) 
+        {
+            if (piles[i] % div == 0)
+            {
+                sum += piles[i] / div;
+            }
+            else
+            {
+                sum += (piles[i] / div) + 1;
+            }
+        }
+        div++;
+    }
+    return div - 1;
+
+}
+int[] piles = { 332484035, 524908576, 855865114, 632922376, 222257295, 690155293, 112677673, 679580077, 337406589, 290818316, 877337160, 901728858, 679284947, 688210097, 692137887, 718203285, 629455728, 941802184 };
+MinEatingSpeed(piles, 823855818);
---
+++ Search 2D Matrix/Program.cs
@@ -0,0 +1,52 @@
+﻿//You are given an m x n integer matrix matrix with the following two properties:
+
+//Each row is sorted in non-decreasing order.
+//The first integer of each row is greater than the last integer of the previous row.
+//Given an integer target, return true if target is in matrix or false otherwise.
+
+//You must write a solution in O(log(m * n)) time complexity.
+//    Example 1: 
+//    Input: matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target = 3
+//Output: true
+//    Example 2:
+//    Input: matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target = 13
+//Output: false
+bool SearchMatrix(int[,] matrix, int target)
+{
+    if (matrix[0, 0] < target || matrix[matrix.GetLength(0), matrix.GetLength(1)] > target) return false;
+    int i = 0;
+    while(i < matrix.Rank && (target > matrix[i, matrix.GetLength(0)]))
+    {
+        i++;
+    }
+    int[] nums = new int[matrix.GetLength(0)];
+    for(int k=0; k<matrix.GetLength(0);k++)
+    {
+        nums[k] = matrix[i, k];
+    }
+    return Search(nums, target);
+
+}
+bool Search(int[] nums, int target)
+{
+
+    int left = 0;
+    int right = nums.Length - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) return true;
+        if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return false;
+
+}
+int[,] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+SearchMatrix(matrix, 6);
